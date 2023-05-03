cd66b1b4072647583e1e0bd73978bff2a2583161
nahid139 <nahidnhptake@gmail.com>
2023-05-03 05:38:38 -0700

    chef and recipe json added

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
 const express = require('express');
-const cors =require ("cors");
-
 const app = express();
+const cors = require('cors');
+const port = process.env.PORT || 5000;
+
+const chef = require('./chef.json');
+const recipes = require('./recipes.json');
+
 app.use(cors());
-const allServices =require('./data.json');
 
-app.get('/',(req,res)=>{
-    res.json({message:'hello server running'})
-});
-app .get("/allData",(req, res) =>{
-res.send({allServices});
+app.get('/', (req, res) => {
+    res.send('Dragon is running')
 });
 
-app.listen(5000, ()=>{
-    console.log('server is running')
-});
+app.get('/chef', (req, res) => {
+    res.send(chef);
+})
+
+app.get('/recipes', (req, res) => {
+    res.send(recipes);
+})
+
+app.get('/chef/:id', (req, res) => {
+    const id = req.params.id;
+    const selectedChef = news.find(n => n._id === id);
+    res.send(selectedChef)
+})
+
+app.get('/recipes/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    if (id === 0) {
+        res.send(recipes)
+    }
+    else {
+        const categoryRecipes = news.filter(n => parseInt(n.category_id) === id);
+        res.send(categoryRecipes)
+    }
+
+})
+
+app.listen(port, () => {
+    console.log(`Dragon API is running on port: ${port}`)
+})
